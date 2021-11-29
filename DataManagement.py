9315b666106dabc6861bce6d576613a8c781b48f
Carmine <carmine.schipani@epfl.ch>
2021-11-29 16:30:24 +0100

    Parent abstract class DataManagement.py created

---
+++ DataManagement.py
@@ -0,0 +1,28 @@
+import numpy as np
+from abc import ABC
+
+class DataManagement(ABC):
+    """Abstract parent class for data management. Using the associated MATLAB 
+    LOAD_CLASS.m
+    class, the postprocessing in MATLAB is simplified and reliable because the parameters from the OpenSeesPy analysis are imported automatically. 
+    """
+
+    def SaveData(self, f):
+        """Function that list in the command window and saves in a opened file text "f" the data from the "self" class that calls it. 
+        Normally, call this function after this line:
+        with open(FileName, 'w') as f:
+
+        Args:
+            f (io.TextIOWrapper): Opened file to write into
+        """
+
+        delimiter = "##############################" # 30 times #
+        for data_line in self.data:
+            f.write('\n')
+            if type(data_line) == np.ndarray:
+                tmp_str = np.array_str(data_line, max_line_width = np.inf)
+            else:
+                tmp_str = str(data_line)
+            f.write(tmp_str)
+        f.write('\n')
+        f.write(delimiter)
