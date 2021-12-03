38a86b6c4e30896b29126e856428674dccc389a1
Carmine <carmine.schipani@epfl.ch>
2021-12-03 16:49:47 +0100

    Implemented ZeroDivision, NegativeValue, PositiveValue and WrongArgument type of error

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -2,7 +2,22 @@
 #   Carmine Schipani, 2021
 
 class ZeroDivision(Exception):
-    """Exception class for the zero division error.
+    """Exception class for the "zero division" error.
+    """
+    pass
+
+class WrongArgument(Exception):
+    """Exception class for the "input of a wrong argument" error.
+    """
+    pass
+
+class NegativeValue(Exception):
+    """Exception class for the "negative value (argument or result)" error.
+    """
+    pass
+
+class PositiveValue(Exception):
+    """Exception class for the "positive value (argument or result)" error.
     """
     pass
 
