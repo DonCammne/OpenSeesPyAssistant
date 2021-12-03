2740091b9ff4d7a1a10bffd2596bddbe83700c69
Carmine <carmine.schipani@epfl.ch>
2021-12-03 16:46:46 +0100

    Module Constants implemented

---
+++ Constants.py
@@ -0,0 +1,10 @@
+# Module that set the value for a set of essential constants
+#   Carmine Schipani, 2021
+
+"""Module with the values of a set of essential constants. 
+"""
+
+TOL = 1.0e-6
+ZERO = 1.0e-9
+G_CONST = 9.810
+RIGID = 100.0 			# multiply with the biggest value to have an infinitely rigid element
