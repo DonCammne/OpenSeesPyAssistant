f8c1c235fca1631856b2ac65728f6d2f7f1f08e6
Carmine <carmine.schipani@epfl.ch>
2021-12-29 18:23:04 +0100

    Add MAX_ITER constant

--- Constants.py
+++ Constants.py
@@ -7,6 +7,7 @@ from OpenSeesPyHelper.Units import *
 """
 
 TOL = 1.0e-6
-ZERO = 1.0e-9
+ZERO = 1.0e-9 # used when defining mass that is equal to 0 (avoid convergence problem)
 G_CONST = 9.810*m_unit/s_unit**2
 RIGID = 100.0 			# multiply with the biggest value to have an infinitely rigid element
+MAX_ITER = 100
